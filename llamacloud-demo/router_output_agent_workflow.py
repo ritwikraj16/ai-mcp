d617b33b79b649fdfd660e1139cdc804c59cedc5
learningmachine98 <supreeth.reddy98@gmail.com>
2025-03-14 22:37:38 -0400

    Added file router output

---
+++ llamacloud-demo/router_output_agent_workflow.py
@@ -0,0 +1,160 @@
+from typing import Dict, List, Any, Optional
+
+from llama_index.core.tools import BaseTool
+from llama_index.core.llms import ChatMessage
+from llama_index.core.llms.llm import ToolSelection, LLM
+from llama_index.core.workflow import (
+    Workflow,
+    Event,
+    StartEvent,
+    StopEvent,
+    step,
+    Context
+)
+from llama_index.core.base.response.schema import Response
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.openai import OpenAI
+
+
+class InputEvent(Event):
+    """Input event."""
+    message: Optional[str] = None
+
+class GatherToolsEvent(Event):
+    """Gather Tools Event"""
+
+    tool_calls: Any
+
+class ToolCallEvent(Event):
+    """Tool Call event"""
+
+    tool_call: ToolSelection
+
+class ToolCallEventResult(Event):
+    """Tool call event result."""
+
+    msg: ChatMessage
+
+class RouterOutputAgentWorkflow(Workflow):
+    """Custom router output agent workflow."""
+
+    def __init__(self,
+        tools: List[BaseTool],
+        timeout: Optional[float] = 40.0,
+        disable_validation: bool = False,
+        verbose: bool = False,
+        llm: Optional[LLM] = None,
+        chat_history: Optional[List[ChatMessage]] = None,
+    ):
+        """Constructor."""
+
+        super().__init__(timeout=timeout, disable_validation=disable_validation, verbose=verbose)
+
+        self.tools: List[BaseTool] = tools
+        self.tools_dict: Optional[Dict[str, BaseTool]] = {tool.metadata.name: tool for tool in self.tools}
+        self.llm: LLM = llm or OpenAI(temperature=0, model="gpt-3.5-turbo")
+        self.chat_history: List[ChatMessage] = chat_history or []
+    
+
+    def reset(self) -> None:
+        """Resets Chat History"""
+
+        self.chat_history = []
+
+    @step()
+    async def prepare_chat(self, ev: StartEvent) -> InputEvent:
+        message = ev.get("message")
+        if message is None:
+            raise ValueError("'message' field is required.")
+        
+        # add msg to chat history
+        chat_history = self.chat_history
+        chat_history.append(ChatMessage(role="user", content=message))
+        return InputEvent()
+
+    @step()
+    async def chat(self, ev: InputEvent) -> GatherToolsEvent | StopEvent:
+        """Appends msg to chat history, then gets tool calls."""
+
+        # Put msg into LLM with tools included
+        chat_res = await self.llm.achat_with_tools(
+            self.tools,
+            chat_history=self.chat_history,
+            verbose=self._verbose,
+            allow_parallel_tool_calls=True
+        )
+        tool_calls = self.llm.get_tool_calls_from_response(chat_res, error_on_no_tool_call=False)
+        
+        ai_message = chat_res.message
+        self.chat_history.append(ai_message)
+        if self._verbose:
+            print(f"Chat message: {ai_message.content}")
+
+        # no tool calls, return chat message.
+        if not tool_calls:
+            return StopEvent(result=ai_message.content)
+
+        return GatherToolsEvent(tool_calls=tool_calls)
+
+    @step(pass_context=True)
+    async def dispatch_calls(self, ctx: Context, ev: GatherToolsEvent) -> ToolCallEvent:
+        """Dispatches calls."""
+
+        tool_calls = ev.tool_calls
+        await ctx.set("num_tool_calls", len(tool_calls))
+
+        # trigger tool call events
+        for tool_call in tool_calls:
+            ctx.send_event(ToolCallEvent(tool_call=tool_call))
+        
+        return None
+    
+    @step()
+    async def call_tool(self, ev: ToolCallEvent) -> ToolCallEventResult:
+        """Calls tool."""
+
+        tool_call = ev.tool_call
+
+        # get tool ID and function call
+        id_ = tool_call.tool_id
+
+        if self._verbose:
+            print(f"Calling function {tool_call.tool_name} with msg {tool_call.tool_kwargs}")
+
+        # call function and put result into a chat message
+        tool = self.tools_dict[tool_call.tool_name]
+        output = await tool.acall(**tool_call.tool_kwargs)
+        msg = ChatMessage(
+            name=tool_call.tool_name,
+            content=str(output),
+            role="tool",
+            additional_kwargs={
+                "tool_call_id": id_,
+                "name": tool_call.tool_name
+            }
+        )
+
+        return ToolCallEventResult(msg=msg)
+    
+    @step(pass_context=True)
+    async def gather(self, ctx: Context, ev: ToolCallEventResult) -> StopEvent | None:
+        """Gathers tool calls."""
+        # wait for all tool call events to finish.
+        tool_events = ctx.collect_events(ev, [ToolCallEventResult] * await ctx.get("num_tool_calls"))
+        if not tool_events:
+            return None
+        
+        for tool_event in tool_events:
+            # append tool call chat messages to history
+            self.chat_history.append(tool_event.msg)
+        
+        # Get final response from LLM using updated chat history with tool results
+        chat_response = await self.llm.achat(
+            self.chat_history
+        )
+        
+        # Add the final AI response to chat history
+        self.chat_history.append(chat_response.message)
+
+        # Return the final response as a StopEvent
+        return StopEvent(result=chat_response.message.content) 
